c4918579766832935cf31b516ee2f11880f53ba7
Vladar4 <vladar4@gmail.com>
2017-01-09 20:11:58 +0200

    added assets.nim

---
+++ nimgame2/assets.nim
@@ -0,0 +1,52 @@
+# nimgame2/assets.nim
+# Copyright (c) 2016-2017 Vladar
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Vladar vladar4@gmail.com
+
+import
+  os, math, tables
+
+
+export
+  tables
+
+
+type
+  Assets*[T] = OrderedTableRef[string, T]
+
+
+proc load[T](assets: Assets[T], dir: string, init: proc(file: string): T) =
+  var files: seq[string] = @[]
+  for i in walkDir(dir):
+    if i.kind == pcFile:
+      files.add(i.path)
+  assets[] = initOrderedTable[string, T](nextPowerOfTwo(files.len))
+  for i in files:
+    let name = i.splitFile.name
+    let data = init(i)
+    assets.add(name, data)
+
+
+proc newAssets*[T](dir: string, init: proc(file: string): T): Assets[T] =
+  new result
+  result.load(dir, init)
+
+
